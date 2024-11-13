c86fe90104df1ee0895eec071accfc97f85bac5f
Nisha <nisha971943@gmail.com>
2024-11-13 10:09:10 +0530

    my commit

---
+++ React/Intero.js
@@ -0,0 +1,16 @@
+//f-bolt
+//faxjs
+//React
+//first Deploy->Jordon+Team(Meta)
+//First Version->2013 0.3.0
+//current version -> 18.3.1
+//React is a single page application
+//1.cdn(Content Delivery Network)
+//2.Bundlq
+//CRA(webpack)
+//2.parcel
+//3.vite(esbuild)
+//DIFF ALGORITHM
+//ORIGINAL DOM->VIRTUAL DOM->VIRTUAL DOM
+//tHIS PEOCESS CALLED RECONSILIOATION
+//NEW ALGORITHM FIBER
